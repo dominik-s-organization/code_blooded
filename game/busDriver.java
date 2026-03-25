fbce70dc27cd837e68e10c5584a8118c883e30f6
Latinovits Jakab Jácint <231105171+Jakke20@users.noreply.github.com>
2026-03-25 19:53:50 +0100

    Kommentek Lane-ig

--- game/busDriver.java
+++ game/busDriver.java
@@ -1,9 +1,15 @@
 package game;
 
+/**
+ * A BusDriver osztály a Player absztrakt osztályból származik, és egy buszvezetőt reprezentál a játékban.
+ */
 public class BusDriver extends Player {
+    // A buszvezető által teljesített útvonalak száma.
     private int completedRoutes = 0;
+    // A busz, amelyet a buszvezető vezet.
     private Bus bus;
 
+    // A buszvezető teljesíti az útvonalat, és növeli a teljesített útvonalak számát.
     public void completeRoute() {
         completedRoutes++;
     }
